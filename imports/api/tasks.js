3687f5146dc42ebb83d48fbb84eb8929487e7ed8
ashishnaik021 <iassureit@gmail.com>
2019-02-03 18:37:04 +0530

    Day2 code commit

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -2,4 +2,29 @@ import { Mongo } from 'meteor/mongo';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if(Meteor.isServer){
+	Meteor.publish("tasksData",function(){
+		return Tasks.find({"status":"new"},{sort:{"createdAt":-1}},{limit:30});
+	});
+}
 
+Meteor.methods({
+	"insertTasks" : function(inputTask){
+			Tasks.insert({
+				"task" : inputTask,
+				"status" : "new",
+				"createdAt" : new Date(),
+			},(error,result)=>{
+					if(error){
+						console.log("error = ", error);
+						return error;
+					}
+					if(result){
+						console.log("result = ",result);
+						return result;
+					}
+			});	
+	}
+
+
+});
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,63 +7,148 @@ import { Tasks } from '/imports/api/tasks.js';
 class App extends Component{
 	//======  Section 1 ========
 	// All lifecyle classes
+	constructor(props){
+		super(props);
+
+		this.state = {
+			"inputValue" : "",
+			"taskid" : "",
+			"operation" : "insert",
+			"doneClass" : "",
+		};		
+
+	}
 
 
 	//======  Section 2 ========
 	// User defined events
-	getToDoList(){
-		var todolist = [
-			{id:1, task:"Attend The Great ReactJS Training!"},
-			{id:2, task:"Lets Do Shopping in Amanora Mall"},
-			{id:3, task:"Agent Jacks is Calling"},
-			{id:4, task:"Now Time for Pizza"},
-			{id:5, task:"Going to sleep... Bye!"},
-		];
-
-		return todolist;
-	}
 
 	showTasks(){
-		console.log("this.props.allTasks = ", this.props.allTasks);
 		return this.props.allTasks.map(
 							(t)=>{
-									return( <li key={t._id}> {t.task} </li> );
+									return( <li key={t._id}> 
+															<span id={t._id} className={t.status=="completed"?"done":""}> {t.task} </span>
+															<span id={"del-"+t._id} className="del" onClick={this.deleteTask.bind(this)}> x </span>
+															<span id={"edit-"+t._id} data-task={t.task} className="del" onClick={this.editTask.bind(this)}> E </span>
+															<span id={"done-"+t._id} className="del" onClick={this.doneTask.bind(this)}>{t.status=="completed"?"New":"Done"}</span>
+													</li> );
 							 }
 					 );
 	}
 
-	handleSubmit(event){
+	doneTask(event){
 		event.preventDefault();
-		var inputTask = this.refs.task.value;
-		console.log("inputTask = ",inputTask);
+		var taskid = event.currentTarget.id;
+		var tid = taskid.split("-");
 
-		Tasks.insert({
-			"task" : inputTask,
-			"createdAt" : new Date(),
-		},(error,result)=>{
+		var text = event.currentTarget.textContent;
+
+		if(text == "New"){
+			var status = "new";
+		}else{
+			var status = "completed";
+		}
+
+		Tasks.update(
+			{"_id":tid[1]},
+			{$set : {"status":status} },
+			(error,result)=>{
 				if(error){
-					console.log("error = ", error);
+					console.log("error = ",error);
 				}
 				if(result){
-					console.log("result = ",result);
-					this.refs.task.value = "";
+					console.log("Status Changed to Completed!");
 				}
-		});
+			}
+		);
+
 
 	}
 
+	editTask(event){
+		event.preventDefault();
+		var taskid = event.currentTarget.id;
+		var tid = taskid.split("-");
+
+		var taskVal = event.target.getAttribute("data-task");
+		this.setState({"inputValue" : taskVal, "taskid":tid[1], "operation":"edit"});
+	}
+
+	deleteTask(event){
+		event.preventDefault();
+
+		var r = confirm("Are you sure you want to Delete this Task?")
+		if (r == true) {
+		  var txt = "You pressed OK!";
+			var taskid = event.currentTarget.id; 
+			var t = taskid.split("-");
+			Tasks.remove({"_id":t[1]});
+
+		} else {
+		  var txt = "You pressed Cancel!";
+		}
+
+	}
+
+
+	handleSubmit(event){
+		event.preventDefault();
+		var inputTask = this.refs.task.value;
+		
+		if(this.state.operation == "insert"){
+			Meteor.call("insertTasks",inputTask,
+										(err,rslt)=>{
+											if(err){
+												console.log("Something went wrong! Error = ", error);
+											}
+											if(rslt){
+												alert("Task inserted Successfully!");
+												this.setState({"inputValue":""});
+											}
+										});	
+		}
+
+		if(this.state.operation == "edit"){
+			Tasks.update({"_id":this.state.taskid},
+					{$set: 	{
+										"task" : inputTask,
+										"createdAt" : new Date(),
+									} 
+					}
+					,(error,result)=>{
+						if(error){
+							console.log("error = ", error);
+						}
+						if(result){
+							console.log("result = ",result);
+							alert("Task Edited Successfully");
+							this.setState({"inputValue":"", "operation":"insert","taskid":""});
+						}
+				});			
+
+		}
+
+
+	}
+
+	handleChange(event){
+		event.preventDefault();
+
+		var newInput = event.currentTarget.value;
+		this.setState({"inputValue" : newInput});
+	}
+
+
 	//======  Section 3 ========
 	render(){
-		console.log(this.props);
-
 		return (
 			/*==== Here goes your html ====*/
 			<div className="container">
-	    	<h1> To Do List </h1>
+	    	<h1> To Do List <span className="smallTxt"> ({this.props.completed} completed out of {this.props.total} Tasks) </span> </h1> 
 	    	<hr className="redLine" />
 
 	    	<form onSubmit={this.handleSubmit.bind(this)}>
-	    		<input className="taskInp" type="text" ref="task" placeholder="Enter Your Task..."/>
+	    		<input value={this.state.inputValue} onChange={this.handleChange.bind(this)} className="taskInp" type="text" ref="task" placeholder="Enter Your Task..."/>
 	    	</form>
 
 	    	<ul className='todolist'> 
@@ -76,7 +161,11 @@ class App extends Component{
 
 
 export default withTracker(()=>{
+	Meteor.subscribe("tasksData");
+
 	return {
 		"allTasks" 			: Tasks.find({},{sort:{"createdAt":-1}}).fetch(),
+		"total"					: Tasks.find({}).count(),
+		"completed"			: Tasks.find({"status":"completed"}).count(),
 	}
 })(App);
